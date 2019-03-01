b556650883f3bee75e926125dddd4b465fa9ad6f
gggllm <515815530@qq.com>
2019-03-01 15:47:16 -0500

    add fake ui

---
+++ src/components/Home.tsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import Profile from '../pages/tweeper/Profile'
+
+
+function home(){
+    return (
+        <Profile></Profile>
+    )
+}
+
+export default home
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Navbar from './components/Navbar';
 import Login from './components/Login';
+import Home from './components/Home'
 import * as serviceWorker from './serviceWorker';
 
 
-ReactDOM.render(<Navbar />, document.getElementById('navbar'));
+//ReactDOM.render(<Navbar />, document.getElementById('navbar'));
 ReactDOM.render(<Login />, document.getElementById('body')); 
-
+ReactDOM.render(<Home />, document.getElementById('body')); 
 serviceWorker.register();
