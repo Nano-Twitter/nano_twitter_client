9a9c5067eb9b35d719f9ccc984647c9558d1253a
chauncy <chauncyf@qq.com>
2019-03-20 15:43:57 -0400

    ok

--- src/containers/Register/index.js
+++ src/containers/Register/index.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {observer, inject} from 'mobx-react';
 // import { Link } from 'react-router';
-import { withStyles, createStyles} from '@material-ui/core/styles';
+import {withStyles, createStyles} from '@material-ui/core/styles';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -16,39 +16,39 @@ import Typography from '@material-ui/core/Typography';
 
 const styles = theme =>
 
-  createStyles({
-    main: {
-      width: 'auto',
-      display: 'block', // Fix IE 11 issue.
-      marginLeft: theme.spacing.unit * 3,
-      marginRight: theme.spacing.unit * 3,
-      [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
-        width: 400,
-        marginLeft: 'auto',
-        marginRight: 'auto',
-      },
-    },
-    paper: {
-      marginTop: theme.spacing.unit * 8,
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
-    },
-    avatar: {
-      margin: theme.spacing.unit,
-      backgroundColor: theme.palette.secondary.main,
-    },
-    form: {
-      width: '100%', // Fix IE 11 issue.
-      marginTop: theme.spacing.unit,
-    },
-    submit: {
-      marginTop: theme.spacing.unit * 3,
-    },
-  });
+    createStyles({
+        main: {
+            width: 'auto',
+            display: 'block', // Fix IE 11 issue.
+            marginLeft: theme.spacing.unit * 3,
+            marginRight: theme.spacing.unit * 3,
+            [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+                width: 400,
+                marginLeft: 'auto',
+                marginRight: 'auto',
+            },
+        },
+        paper: {
+            marginTop: theme.spacing.unit * 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+        },
+        avatar: {
+            margin: theme.spacing.unit,
+            backgroundColor: theme.palette.secondary.main,
+        },
+        form: {
+            width: '100%', // Fix IE 11 issue.
+            marginTop: theme.spacing.unit,
+        },
+        submit: {
+            marginTop: theme.spacing.unit * 3,
+        },
+    });
 
-class Register extends Component{
+class Register extends Component {
 
     changeUsername = (e) => {
         this.props.rootStore.registerStore.changeUsername(e.target.value);
@@ -68,53 +68,57 @@ class Register extends Component{
 
     render() {
 
-        const cacheAvatar=false;
+        const cacheAvatar = false;
 
         return (
 
             <main className={this.props.classes.main}>
-                <CssBaseline />
+                <CssBaseline/>
                 <Paper className={this.props.classes.paper}>
-                { cacheAvatar?
-                    <Avatar className={this.props.classes.avatar}>
-                    <LockOutlinedIcon />
-                    </Avatar>
-                :
-                    <Avatar className={this.props.classes.avatar} alt="Remy Sharp" src="https://material-ui.com/static/images/avatar/1.jpg">
-                    </Avatar>
-                }
-                <Typography component="h1" variant="h5">
-                    Register
-                </Typography>
-                <form className={this.props.classes.form}>
-                    <FormControl margin="normal" required fullWidth>
-                    <InputLabel htmlFor="username">Username</InputLabel>
-                    <Input onChange={this.changeUsername} id="username" name="username" type="text" autoComplete="current-username" autoFocus />
-                    </FormControl>
-                    <FormControl margin="normal" required fullWidth>
-                    <InputLabel htmlFor="email">Email Address</InputLabel>
-                    <Input onChange={this.changeEmail} id="email" name="email" type="email" autoComplete="email"/>
-                    </FormControl>
-                    <FormControl margin="normal" required fullWidth>
-                    <InputLabel htmlFor="password">Password</InputLabel>
-                    <Input onChange={this.changePassword} id="password" name="password" type="password" autoComplete="current-password" />
-                    </FormControl>
-                    <FormControlLabel
-                    control={<Checkbox value="remember" color="primary" />}
-                    label="Remember me"
-                    />
-                    <Button
-                    fullWidth
-                    variant="contained"
-                    color="primary"
-                    onClick={this.register}
-                    >
-                    Register
-                    </Button>
-                </form>
+                    {cacheAvatar ?
+                        <Avatar className={this.props.classes.avatar}>
+                            <LockOutlinedIcon/>
+                        </Avatar>
+                        :
+                        <Avatar className={this.props.classes.avatar} alt="Remy Sharp"
+                                src="https://material-ui.com/static/images/avatar/1.jpg">
+                        </Avatar>
+                    }
+                    <Typography component="h1" variant="h5">
+                        Register
+                    </Typography>
+                    <form className={this.props.classes.form}>
+                        <FormControl margin="normal" required fullWidth>
+                            <InputLabel htmlFor="username">Username</InputLabel>
+                            <Input onChange={this.changeUsername} id="username" name="username" type="text"
+                                   autoComplete="current-username" autoFocus/>
+                        </FormControl>
+                        <FormControl margin="normal" required fullWidth>
+                            <InputLabel htmlFor="email">Email Address</InputLabel>
+                            <Input onChange={this.changeEmail} id="email" name="email" type="email"
+                                   autoComplete="email"/>
+                        </FormControl>
+                        <FormControl margin="normal" required fullWidth>
+                            <InputLabel htmlFor="password">Password</InputLabel>
+                            <Input onChange={this.changePassword} id="password" name="password" type="password"
+                                   autoComplete="current-password"/>
+                        </FormControl>
+                        <FormControlLabel
+                            control={<Checkbox value="remember" color="primary"/>}
+                            label="Remember me"
+                        />
+                        <Button
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            onClick={this.register}
+                        >
+                            Register
+                        </Button>
+                    </form>
                 </Paper>
             </main>
-    
+
         );
     }
 }
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,17 +1,18 @@
-import React, { Component } from 'react';
-import { Redirect, Route }from "react-router-dom";
-import { observer, inject } from "mobx-react";
+import React, {Component} from 'react';
+import {Redirect, Route} from "react-router-dom";
+import {observer, inject} from "mobx-react";
 
-class AuthorizedRoute extends Component{
-    render(){
-        let { component: Component, ...rest } = this.props;
-        let { isLoggedIn } = this.props.rootStore.loginStore;
-        return(
-            <Route {...rest} render={ props =>{
+class AuthorizedRoute extends Component {
+    render() {
+        let {component: Component, ...rest} = this.props;
+        let {isLoggedIn} = this.props.rootStore.loginStore;
+        return (
+            <Route {...rest} render={props => {
                 console.log(isLoggedIn);
-                return isLoggedIn==='true'?<Component {...this.props} />:<Redirect to="/login" /> 
+                return isLoggedIn === 'true' ? <Component {...this.props} /> : <Redirect to="/login"/>
             }}/>
         )
     }
 }
+
 export default inject('rootStore')(observer(AuthorizedRoute));
