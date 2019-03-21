9fc12c839197ed610d5f4c18c8a791906197f7f5
chauncy <chauncyf@qq.com>
2019-03-21 15:34:13 -0400

    tweet block

--- src/stores/rootStore/index.js
+++ src/stores/rootStore/index.js
@@ -1,16 +1,18 @@
 import loginStore from '../loginStore'
 import registerStore from '../registerStore';
 import profileStore from '../profileStore';
+import tweetStore from "../tweetStore";
 
 class RootStore {
     constructor() {
-      this.registerStore = registerStore;
-      this.loginStore = loginStore;
-      this.profileStore = profileStore
+        this.registerStore = registerStore;
+        this.loginStore = loginStore;
+        this.profileStore = profileStore;
+        this.tweetStore = tweetStore;
     }
 }
 
 const rootStore = new RootStore();
 
 export default rootStore;
-export { RootStore };
+export {RootStore};
