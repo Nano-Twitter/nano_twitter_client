d43bbc3b8501c69ca59c0c8ea33fb6674bfe9bca
kudari00 <yehong@brandeis.edu>
2019-04-29 17:04:32 -0400

    comment out unused icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import IconButton from '@material-ui/core/IconButton';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
 import InputBase from '@material-ui/core/InputBase';
-import Badge from '@material-ui/core/Badge';
+// import Badge from '@material-ui/core/Badge';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 import { fade } from '@material-ui/core/styles/colorManipulator';
@@ -13,9 +13,9 @@ import { withStyles, createStyles } from '@material-ui/core/styles';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import AccountCircle from '@material-ui/icons/AccountCircle';
-import EditIcon from '@material-ui/icons/Edit'
-import MailIcon from '@material-ui/icons/Mail';
-import NotificationsIcon from '@material-ui/icons/Notifications';
+// import EditIcon from '@material-ui/icons/Edit'
+// import MailIcon from '@material-ui/icons/Mail';
+// import NotificationsIcon from '@material-ui/icons/Notifications';
 import MoreIcon from '@material-ui/icons/MoreVert';
 import { observer, inject } from 'mobx-react';
 import Link from '@material-ui/core/Link';
@@ -198,20 +198,20 @@ class PrimarySearchAppBar extends React.Component {
                 onClose={this.closeAllMenues}
             >
                 <MenuItem onClick={this.handleMobileMenuClose}>
-                    <IconButton color="inherit">
+                    {/* <IconButton color="inherit">
                         <Badge badgeContent={4} color="secondary">
                             <MailIcon />
                         </Badge>
                     </IconButton>
-                    <p>Messages</p>
+                    <p>Messages</p> */}
                 </MenuItem>
                 <MenuItem onClick={this.handleMobileMenuClose}>
-                    <IconButton color="inherit">
+                    {/* <IconButton color="inherit">
                         <Badge badgeContent={11} color="secondary">
                             <NotificationsIcon />
                         </Badge>
                     </IconButton>
-                    <p>Notifications</p>
+                    <p>Notifications</p> */}
                 </MenuItem>
                 <MenuItem onClick={this.handleProfileMenuOpen}>
                     <IconButton color="inherit">
@@ -250,19 +250,19 @@ class PrimarySearchAppBar extends React.Component {
                         </div>
                         <div className={classes.grow} />
                         <div className={classes.sectionDesktop}>
-                            <IconButton color="inherit">
+                            {/* <IconButton color="inherit">
                                 <EditIcon />
-                            </IconButton>
-                            <IconButton color="inherit">
+                            </IconButton> */}
+                            {/* <IconButton color="inherit">
                                 <Badge badgeContent={1} color="secondary">
                                     <MailIcon />
                                 </Badge>
-                            </IconButton>
-                            <IconButton color="inherit">
+                            </IconButton> */}
+                            {/* <IconButton color="inherit">
                                 <Badge badgeContent={1} color="secondary">
                                     <NotificationsIcon />
                                 </Badge>
-                            </IconButton>
+                            </IconButton> */}
                             <IconButton
                                 aria-owns={isProfileMenuOpen ? 'material-appbar' : undefined}
                                 aria-haspopup="true"
