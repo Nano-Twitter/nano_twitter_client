35d1388daef79678458b740a02f1fac1d9acbcc4
kudari00 <yehong@brandeis.edu>
2019-04-29 16:55:40 -0400

    large name card

--- src/components/tweet/NameCardLarge.js
+++ src/components/tweet/NameCardLarge.js
@@ -1,101 +1,110 @@
 import React, { Component } from 'react';
 import {observer, inject} from 'mobx-react';
 import { withStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
+import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import Avatar from '@material-ui/core/Avatar';
+import indigo from '@material-ui/core/colors/indigo';
+import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
+import { Button } from '@material-ui/core';
+
 
 const styles = theme => ({
   main: {
     width: 'auto',
-    marginLeft: theme.spacing.unit * 3,
-    marginRight: theme.spacing.unit * 3,
     paddingTop: theme.spacing.unit * 2
   },
-  appBar: {
-    position: 'relative',
+  margin: {
+    margin: theme.spacing.unit,
   },
-  icon: {
-    marginRight: theme.spacing.unit * 2,
-  },
-  heroUnit: {
-    backgroundColor: theme.palette.background.paper,
-  },
-  heroContent: {
-    maxWidth: 600,
-    margin: '0 auto',
-    padding: `${theme.spacing.unit * 8}px 0 ${theme.spacing.unit * 6}px`,
-  },
-  heroButtons: {
-    marginTop: theme.spacing.unit * 4,
-  },
-  layout: {
-    width: 'auto',
-    marginLeft: theme.spacing.unit * 3,
-    marginRight: theme.spacing.unit * 3,
-    [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
-      width: 1100,
-      marginLeft: 'auto',
-      marginRight: 'auto',
-    },
-  },
-  cardGrid: {
-    padding: `${theme.spacing.unit * 8}px 0`,
+  extendedIcon: {
+    marginRight: theme.spacing.unit,
   },
   card: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
+    // maxWidth: 345,
   },
-  cardMedia: {
-    paddingTop: '56.25%', // 16:9
+  media: {
+    height: 80,
+    backgroundColor: indigo[500],
   },
-  cardContent: {
-    flexGrow: 1,
+  bigAvatar: {
+    marginTop: -53,
+    width: 75,
+    height: 75,
+    border: '3px solid #ffffff',
   },
-  footer: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing.unit * 6,
+  profile: {
+    paddingTop:5,
+    paddingBottom:5,
+    height: 100
   },
+  link: {
+    color: indigo[400]
+  },
+  names:{
+    paddingLeft: 5,
+    paddingTop: 5
+  },
+  username:{
+    fontSize: 18
+  },
+  atname:{
+    fontSize: 14
+  }
 });
-
-
 
 class NameCardLarge extends Component {
     render(){
-      const { classes } = this.props;
         return (
-            <main className={this.props.classes.main}>
-              <div className={(classes.layout, classes.cardGrid)}>
-                <Card className={classes.card}>
-                  <CardMedia
-                    className={classes.cardMedia}
-                    image="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164edaf95ee%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164edaf95ee%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.32500076293945%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" // eslint-disable-line max-len
-                    title="Image title"
-                  />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Heading
-                    </Typography>
-                    <Typography>
-                      This is a media card. You can use this section to describe the content.
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small" color="primary">
-                      View
-                    </Button>
-                    <Button size="small" color="primary">
+          <Card className={this.props.classes.card}>
+            <CardActionArea>
+              <CardMedia
+                className={this.props.classes.media}
+                image=""
+                title="Contemplative Reptile"
+              />
+              <CardContent>
+
+                <Grid container spacing={10}>
+                  <Grid item xs={8} md={8} lg={8}>
+                    <Avatar alt={this.props.rootStore.profileStore.username} src="https://material-ui.com/static/images/avatar/1.jpg" className={this.props.classes.bigAvatar} />
+                  </Grid>
+                  <Grid item xs={4} md={4} lg={4}>
+                    <Button
+                      variant="outlined"
+                      size="small"
+                      color="primary"
+                      aria-label="Add"
+                      className={this.props.margin}
+                    >
                       Follow
                     </Button>
-                  </CardActions>
-                </Card>
-              </div>
-            </main>
+                  </Grid>
+                  <Grid item xs={12} md={12} lg={12} className={this.props.classes.names}>
+                    <Typography variant="title" className={this.props.classes.username} >
+                      {this.props.rootStore.profileStore.username}
+                    </Typography>
+                    <Typography gutterBottom variant="caption" className={this.props.classes.atname}>
+                      {'@'}{this.props.rootStore.profileStore.nickname}
+                    </Typography>
+                  </Grid>
+                  
+                </Grid>
+
+                <Grid container spacing={12} className={this.props.classes.profile}>
+                  <Typography color="textPrimary">
+                    A short introduction of this person, if there is one.
+                    <Link>#ProductManager #Microsoft #TechAndArt</Link>
+                  </Typography>
+                </Grid>
+                
+              </CardContent>
+            </CardActionArea>
+          </Card>
             
         );
     }
